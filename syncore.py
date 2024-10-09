2a3e402a9124f731300c37ef1103503ba5177872
Renz Paul Fababeir <renzpaul.fababeir01@gmail.com>
2024-10-10 02:35:07 +0800

    changes

--- syncore.py
+++ syncore.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/patient_management')
+def patient_management():
+    return render_template('patient_management.html')  # Placeholder for patient management page
+
+@app.route('/admin_control')
+def admin_control():
+    return render_template('admin_control.html')  # Placeholder for patient management page
+
+
 if __name__ == '__main__':
     app.run(debug=True)
