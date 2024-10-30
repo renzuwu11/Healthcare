53483f748c4d1dba13836ea88f40456894604f6f
Renz Paul Fababeir <renzpaul.fababeir01@gmail.com>
2024-10-30 20:37:38 +0800

    up

--- syncore.py
+++ syncore.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+from flask import Flask, render_template
 import psycopg2
 import os
-import requests
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -34,38 +33,6 @@ def index():
 def admin_dashboard():
     return render_template('admin_dashboard.html')
 
-# @app.route('/fms')
-# def fms():
-#     connection = get_db_connection()
-#     if not connection:
-#         flash("Database connection error.")
-#         return redirect(url_for('index'))
-
-#     authcursor = connection.cursor()
-#     try:
-#         # Fetch users with roles specific to FMS (Finance Manager, Billing Specialist, Claims Specialist)
-#         query = """
-#         SELECT u.username, a.roleName, u.accountLocked 
-#         FROM USERS u
-#         JOIN AUTHORIZATIONS a ON u.authorizationId = a.authorizationId
-#         WHERE a.roleName IN ('Finance Manager', 'Billing Specialist', 'Claims Specialist')
-#         """
-#         authcursor.execute(query)
-#         users = authcursor.fetchall()
-
-#         users_data = [
-#             {'username': user[0], 'role': user[1], 'status': 'Active' if not user[2] else 'Inactive'}
-#             for user in users
-#         ]
-
-#         return render_template('fms.html', users=users_data)
-#     except Exception as e:
-#         flash(f'An error occurred: {e}')
-#         return redirect(url_for('index'))
-#     finally:
-#         authcursor.close()
-#         connection.close()
-
 @app.route('/fms')
 def fms():
     return render_template('fms.html')
@@ -77,11 +44,6 @@ def pms():
 @app.route('/lms')
 def lms():
     return render_template('lms.html')
-
-# @app.route('/logout', methods=['POST'])
-# def logout():
-#     session.clear()
-#     return jsonify({"redirect": "https://syncore.onrender.com"}), 200
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 3000))
